8c73295eb115d4e7b940d6ec2610862f1efb11eb
Administrator <2390808044@qq.com>
2024-10-13 23:01:42 +0800

    第一次提交

---
+++ ruige-auth/src/main/java/auth/RuiGeAuthApplication.java
@@ -0,0 +1,17 @@
+package auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zhao_rui
+ * @Desc:
+ * @create: 2024-10-08 14:26
+ **/
+
+@SpringBootApplication
+public class RuiGeAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuiGeAuthApplication.class, args);
+    }
+}
---
+++ ruigeforum-gatway/src/main/java/org/example/ruigeforum/gatway/RuigeForumGatwayApplication.java
@@ -0,0 +1,16 @@
+package org.example.ruigeforum.gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zhao_rui
+ * @Desc:
+ * @create: 2024-10-13 21:11
+ **/
+@SpringBootApplication
+public class RuigeForumGatwayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuigeForumGatwayApplication.class, args);
+    }
+}
